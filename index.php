c33c7057632b99d0c30be11fcbf85ca27716cac1
Tjoeser <86958571+Tjoeser@users.noreply.github.com>
2021-12-08 09:49:28 +0100

    navbar begin

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <html>
+    <head>
+        <link rel="stylesheet" href="media/style.css">
+        <ul class="ul">
+            <li><a class="active" href="#home">Home</a></li>
+            <li><a class="link" href="#news">News</a></li>
+            <li><a class="link" href="#contact">Contact</a></li>
+            <li><a class="link" href="#about">About</a></li>
+</ul>
+    </head>
     <body>
         <h1>testeltyje</h1>
         <p>test</p>
