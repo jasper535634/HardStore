2ded22763d313d0efe8b8caf06763c5147d0a0f5
jasper535634 <535634@edu.rocmn.nl>
2021-12-08 09:54:10 +0100

    index gefixt

--- index.php
+++ index.php
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
-<html>
-    <head>
-        <link rel="stylesheet" href="media/style.css">
-    </head>
-    <body>
-        <h1>testeltyje</h1>
-        <p>test</p>
-    </body>
-</html>
-=======
+ 
 <?php include "header.php"?>
 <article>
     <p>dit is de index pagina</p>
 </article>
 <?php include "footer.php"?>
->>>>>>> 7c78eccea0ab87b935b858baa56b548be2819347
+
