345e39644a88c14985767b14f7f5964f5e078dfc
jasper535634 <535634@edu.rocmn.nl>
2021-12-08 09:54:35 +0100

    gefixt

--- header.php
+++ header.php
@@ -1,9 +1,10 @@
 <html>
     <head>
+    <link rel="stylesheet" href="media/style.css">
     <title>HardStore</title>
     </head>
     <body>
     <header>
         <p>hallo dit is de header</p>
-        
+
     </header>
