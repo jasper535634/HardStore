7aa75dc8f292757ac2703c9e9cb2bd182e6c696d
Tjoeser <86958571+Tjoeser@users.noreply.github.com>
2021-12-14 13:03:09 +0100

    Werkende navbar

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php include "header.php"?>
 <?php include "navbar.php"?>
+<body>
+
+
 <article>
     <p>dit is de index pagina</p>
 </article>
+
+</body>
 <?php include "footer.php"?>
 
--- navbar.php
+++ navbar.php
@@ -1,5 +1,6 @@
-<ul class="ul">
+<ul class="navbar">
             <li><a class="active" href="#home">Home</a></li>
             <li><a class="link" href="#news">News</a></li>
             <li><a class="link" href="#contact">Contact</a></li>
             <li><a class="link" href="#about">About</a></li>
+</ul>
