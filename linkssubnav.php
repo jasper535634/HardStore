c88f131bbe7eccf86c9737716ef002a331e5fb87
hazem <566508@edu.rocmn.nl>
2021-12-14 19:47:49 +0100

    recht en links subnav klaar

---
+++ linkssubnav.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" href="style.css">
+<div class="parent">
+  <!-- <p>A line of text</p> -->
+  <div class="child"></div>
+  <!-- <p>More text :D</p> -->
+</div>
+
+
+<div class=sublinks >
+<a href="#">link1</a><br>
+<br>
+<a href="#">link2</a><br>
+<br>
+<a href="#">link3</a><br>
+
+</div>
---
+++ rechtssubnav.php
@@ -0,0 +1,16 @@
+<?php include "linkssubnav.php";?>
+<link rel="stylesheet" href="./media/style.css">
+<div class="parent2">
+  <!-- <p>A line of text</p> -->
+  <div class="child2" data-link="#"></div>
+  <div class="child3"></div>
+</div>
+
+
+<div class='sublinks2' >
+<a href='#'>inlog pagina</a>
+</div>
+
+<div class='sublinks3' >
+<a href='#'>winkelmand</a>
+</div>
