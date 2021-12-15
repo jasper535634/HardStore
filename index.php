64a50c2a0642bc406d100796fb01f6daf590fffc
jasper535634 <535634@edu.rocmn.nl>
2021-12-15 09:43:01 +0100

    index geupdate

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php include "header.php"?>
 <?php include "linkssubnav.php"?>
 <body>
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 35b7679cb15da5e6080e001edbe11242ba39b6e2
 <article>
     <p>Content vlak</p>
 </article>
