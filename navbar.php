1b42ffe7c10eac7bd35a8be4a53a65eeec06b5c8
Gilbert563307 <94962686+Gilbert563307@users.noreply.github.com>
2022-01-11 13:24:20 +0100

    changes winkelmand

--- navbar.php
+++ navbar.php
@@ -4,4 +4,5 @@
             <li><a class="link" href="#contact">Contact</a></li>
             <li><a class="link" href="#about">About</a></li>
             <li><a class="login" href="#login">login</a></li>
+            <li><a class="winkelm" href="winkelmand.php"><img src="./media/test2.jpg"></a></li>
 </ul>
