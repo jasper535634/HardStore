4a3829adf06830165d3948483cafb63ebc2cd6c2
Tjoeser <86958571+Tjoeser@users.noreply.github.com>
2022-01-12 09:02:59 +0100

    Alle styles geinclude

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php include "header.php"?>
+<link rel="stylesheet" href="media/stylehazem.css">
+<link rel="stylesheet" href="media/stylejasper.css">
+<link rel="stylesheet" href="media/stylegilbert.css">
+<link rel="stylesheet" href="media/stylethijs.css">
 <body>
 
 <section>
