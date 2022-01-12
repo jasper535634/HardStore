0d68868c326a4cc1686b77ab79f9d433b2fce8a6
jasper535634 <535634@edu.rocmn.nl>
2022-01-12 09:40:19 +0100

    geprobeerd logo te maken

--- header.php
+++ header.php
@@ -4,11 +4,12 @@
     <title>HardStore</title>
     </head>
     <body>
-    <header>
-            <div class="headerkop">
-            <h1>HardStore</h1>
-            <!--<img class="logo" src="media/hardstore.png" alt="hardstore">-->
-            </div>
+    <header >
             
+<h1 id="hardstore">HardStore</h1>
+            
+        
+<img class="logoimg" src="media/hardstore.png" alt="hardstore">
+        
     <?php include "navbar.php";?>
     </header>
