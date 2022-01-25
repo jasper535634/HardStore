e837a4c12ce5dac5d1f158b49565d9e6604968af
jasper535634 <535634@edu.rocmn.nl>
2022-01-25 13:33:02 +0100

    shop geprobeerd te maken

--- shop.php
+++ shop.php
@@ -6,7 +6,23 @@
 <?php include "linkssubnav.php"?>
 
 <article>
-    <p>Dit is de shop pagina</p>
+    <div class="flex_container">
+        <div class="container">
+            <div class="content"> <img src="https://via.placeholder.com/250" alt="products"> </div>
+            <div class="content"><p>info over products</p></div>
+            <div class="content"><p>prijs vooraad in winkelmantje</p></div>
+        </div>
+        <div class="container">
+            <div class="content"> <img src="https://via.placeholder.com/250" alt="products"> </div>
+            <div class="content"><p>info over products</p></div>
+            <div class="content"><p>prijs vooraad in winkelmantje</p></div>
+        </div>
+        <div class="container">
+            <div class="content"> <img src="https://via.placeholder.com/250" alt="products"> </div>
+            <div class="content"><p>info over products</p></div>
+            <div class="content"><p>prijs vooraad in winkelmantje</p></div>
+        </div>
+    </div>
 </article>
 </section>
 
